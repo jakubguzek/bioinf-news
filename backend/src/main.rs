808c915934a743b7d53e3c21e250d3688c37120f
jakubguzek <jakub.j.guzek@gmail.com>
2023-04-20 16:26:36 +0200

    add: example for return html string

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,10 @@
-use axum::{routing::get, Router};
-use std::net::SocketAddr;
+use axum::{routing, response};
 
 /// Jsut a quick example main with tokio and axum Router.
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let app = axum::Router::new().route("/", routing::get(root));
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 8080));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -13,6 +12,6 @@ async fn main() {
 }
 
 // Bare-bones test root
-async fn root() -> &'static str {
-    "Hello World!"
+async fn root() -> response::Html<&'static str>{
+    response::Html("<h1>Hello World!</h1>")
 }
