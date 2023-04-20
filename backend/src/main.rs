6679d25618df961f4fedf6d4e7769edf671049f7
jakubguzek <jakub.j.guzek@gmail.com>
2023-04-20 15:42:42 +0200

    add: bare-bones example get for root of the page

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+/// Jsut a quick example main with tokio and axum Router.
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// Bare-bones test root
+async fn root() -> &'static str {
+    "Hello World!"
 }
