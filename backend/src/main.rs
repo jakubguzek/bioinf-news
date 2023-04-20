c5ef13538d1c0d5a2171341dbcac9f5245e13b6f
jakubguzek <jakub.j.guzek@gmail.com>
2023-04-20 23:14:46 +0200

    add: initial fetching of Springer articles
    I added a small module for fetching metadata of bioinformatics articles
    from Springer. For now the endpoint from which the data is fetched is
    hard-coded. I'm getting used to tokio and reqwest.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,24 @@
-use axum::{routing, response};
+use axum::{routing, response::{self, IntoResponse}};
 
-/// Jsut a quick example main with tokio and axum Router.
+mod springer_data;
+
+/// Just a quick example main with tokio and axum Router.
 #[tokio::main]
 async fn main() {
-    let app = axum::Router::new().route("/", routing::get(root));
+    // Get request on /springer endpoint.
+    let app = axum::Router::new().route("/springer", routing::get(springer));
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("-> Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-// Bare-bones test root
-async fn root() -> response::Html<&'static str>{
-    response::Html("<h1>Hello World!</h1>")
+// Test returning response json from Springer API.
+async fn springer() -> response::Response {
+    match springer_data::load_data().await {
+        Ok(json) => response::Json::from(json).into_response(),
+        Err(_) => axum::http::StatusCode::INTERNAL_SERVER_ERROR.into_response()
+    }
 }
