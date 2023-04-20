e408a7c0c92ad46af5768288d1f2108090d2c3c1
jakubguzek <jakub.j.guzek@gmail.com>
2023-04-20 23:14:46 +0200

    add: initial fetching of Springer articles
    I added a small module for fetching metadata of bioinformatics articles
    from Springer. For now the endpoint from which the data is fetched is
    hard-coded. I'm getting used to tokio and reqwest.

---
+++ backend/src/springer_data.rs
@@ -0,0 +1,24 @@
+// API key required to make requests to Springer API I generated it without 
+// problems but it's not clear to me how it works, and if it's all free or not.
+***REMOVED***
+
+// Function used to return parsed url object.
+fn bioinformatics_articles() -> reqwest::Url {
+    // For now its hard-coded because I was testing how the API is supposed to work.
+    let url = format!("https://api.springernature.com/meta/v2/json?api_key={API_KEY}&q=subject:Bioinformatics+type:Journal+onlinedatefrom:2023-04-01+onlinedateto:2023-04-30+sort:date&s=1&p=100");
+    reqwest::Url::parse(&url).unwrap()
+}
+
+// Function for making the acutal request. Async for the future when we will be 
+// possibly making much more requests
+async fn request(client: &reqwest::Client) -> Result<reqwest::Response, reqwest::Error> {
+    client.get(bioinformatics_articles()).send().await
+}
+
+// this function makes requests and returns serialized value.
+pub async fn load_data() -> Result<serde_json::Value, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let res = request(&client).await?;
+    let body = res.text().await?;
+    Ok(serde_json::from_str(&body).unwrap())
+}
