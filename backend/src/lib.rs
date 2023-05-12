f4a701c49f29639e28ac045c5ec700a5e2f352bd
SIIIKOR <siiikor.sikorski@gmail.com>
2023-05-12 19:19:56 +0200

    cleaned after testing

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -7,15 +7,7 @@ use axum::response::{self, IntoResponse};
 // Test returning response json from Springer API.
 pub async fn springer() -> response::Response {
     match springer_data::load_data().await {
-        Ok(json) => {
-            for i in 0..100 {
-                dbg!(i);
-                dbg!(record::Record::new(
-                   json.get("records").unwrap().get(i).unwrap()
-                ));
-            }
-            response::Json::from(json).into_response()
-        }
+        Ok(json) => response::Json::from(json).into_response(),
         Err(_) => axum::http::StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 }
