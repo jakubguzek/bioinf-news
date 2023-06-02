4603b7e88ef77ba331109618ed4ea8531b1c070d
SIIIKOR <siiikor.sikorski@gmail.com>
2023-06-02 14:08:20 +0200

    added indexes for faster queries

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,10 +6,11 @@ use tokio;
 async fn main() {
     dotenv().ok();
     let client = backend::database::connect_mongo_db().await.unwrap();
-    // let db = client.database("bioinf-news");
-    // backend::update_articles_springer(&db, 1000, 100)
-    //     .await
-    //     .unwrap();
+    backend::database::create_indexes(&client).await;
+    let db = client.database("bioinf-news");
+    backend::update_articles_springer(&db, 1000, 100)
+        .await
+        .unwrap();
 
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("-> Listening on {}", addr);
