edb3698349879db41f9e3690b8508087adb1c40e
jakubguzek <jakub.j.guzek@gmail.com>
2023-06-18 19:52:22 +0200

    change: move code from ArticleList to Article and cleanup

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,27 +1,29 @@
 import React from "react"
 
-function Article(props) {
-  const {item, index} = props;
-  
-  const [article, setArticle] = React.useState({});
-  const [visible, setVisible] = React.useState(false);
+export default function Article(props) {
+  const { item, index } = props;
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  function changeVisibility() {
+    setIsVisible(prevIsVisible => !prevIsVisible)
+  }
 
   return (
-      <div>
-        <button className="article-collapsed-button" onClick={console.log("Click")}>
-          <div className="article-collapsed-entry" key={item.doi}>
-            <span className="index">{index + 1}.</span>
-            <span className="title">{item.title}</span>
-            <span className="pub-date">{item.publication_date}</span>
-            <span className="source">{item.source}</span>
-          </div>
-          {visible[index] && <div className="article-entry">
-            <hr/>
-            <p className="authors"><b>Authors:</b> {article[index].authors}</p>
-            <p className="abstract"> <b>Abstract:</b> {article[index].article_abstract}</p>
-            <p className="keywords"> <b>Keywords::</b> {article[index].key_words}</p>
-          </div>}
-        </button>
-      </div>
+    <div>
+      <button className="article-collapsed-button" onClick={changeVisibility}>
+        <div className="article-collapsed-entry" key={item.doi}>
+          <span className="index">{index + 1}.</span>
+          <span className="title">{item.title}</span>
+          <span className="pub-date">{item.publication_date}</span>
+          <span className="source">{item.source}</span>
+        </div>
+        {isVisible && <div className="article-entry">
+          <hr />
+          <p className="authors"><b>Authors:</b> {item.authors}</p>
+          <p className="abstract"> <b>Abstract:</b> {item.article_abstract}</p>
+          <p className="keywords"> <b>Keywords::</b> {item.key_words}</p>
+        </div>}
+      </button>
+    </div>
   )
 }
--- frontend/src/components/ArticleList.js
+++ frontend/src/components/ArticleList.js
@@ -1,38 +1,12 @@
 import React from "react";
+import Article from "./Article"
 
 export default function ArticleList(props) {
-  const [visible, setVisible] = React.useState(new Array(props.items.length).fill(false))
-
-  function showDetails(id, index) {
-    const v = structuredClone(visible);
-    if (v[index]) {
-      v[index] = false;
-    } else {
-      v[index] = true;
-    }
-    setVisible(v);
-  }
-
   return (
     <div className="article-list">
       <h1> Recent papers in bioinformatics </h1>  {
         props.items.map((item, index) => (
-          <div>
-            <button className="article-collapsed-button" onClick={() => showDetails(item._id, index)}>
-              <div className="article-collapsed-entry" key={item.doi}>
-                <span className="index">{index + 1}.</span>
-                <span className="title">{item.title}</span>
-                <span className="pub-date">{item.publication_date}</span>
-                <span className="source">{item.source}</span>
-              </div>
-              {visible[index] && <div className="article-entry">
-                <hr/>
-                <p className="authors"><b>Authors:</b> {item.authors}</p>
-                <p className="abstract"> <b>Abstract:</b> {item.article_abstract}</p>
-                <p className="keywords"> <b>Keywords::</b> {item.key_words}</p>
-              </div>}
-            </button>
-          </div>
+          <Article item={item} index={index}/>
         ))
       }
     </div>
