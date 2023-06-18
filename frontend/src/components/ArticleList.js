30c2d89a9fd00829c2e5fdc24011da0f1a6920b5
jakubguzek <jakub.j.guzek@gmail.com>
2023-06-18 19:35:17 +0200

    update: frontend to work with backend changes

--- frontend/src/components/ArticleList.js
+++ frontend/src/components/ArticleList.js
@@ -1,13 +1,9 @@
 import React from "react";
 
 export default function ArticleList(props) {
-  const [articles, setArticles] = React.useState(new Array(props.items.length).fill(""));
   const [visible, setVisible] = React.useState(new Array(props.items.length).fill(false))
 
   function showDetails(id, index) {
-    if (!articles[index]) {
-      get_article(id, index)
-    }
     const v = structuredClone(visible);
     if (v[index]) {
       v[index] = false;
@@ -15,23 +11,6 @@ export default function ArticleList(props) {
       v[index] = true;
     }
     setVisible(v);
-  }
-
-  async function get_article(id, index) {
-    const article = await fetch(`http://127.0.0.1:8080/articles/${id}`)
-      .then(res => res.json());
-    const updatedArticles = structuredClone(articles);
-    updatedArticles[index] = article;
-    setArticles(updatedArticles);
-    console.log(updatedArticles);
-  }
-
-  function formatTextFromArray(arr) {
-    try {
-      return arr.map(item => `${item}, `)
-    } catch(err) {
-      console.log(err)
-    }
   }
 
   return (
@@ -48,9 +27,9 @@ export default function ArticleList(props) {
               </div>
               {visible[index] && <div className="article-entry">
                 <hr/>
-                <p className="authors"><b>Authors:</b> {formatTextFromArray(articles[index].authors)}</p>
-                <p className="abstract"> <b>Abstract:</b> {articles[index].article_abstract}</p>
-                <p className="keywords"> <b>Keywords::</b> {formatTextFromArray(articles[index].key_words)}</p>
+                <p className="authors"><b>Authors:</b> {item.authors}</p>
+                <p className="abstract"> <b>Abstract:</b> {item.article_abstract}</p>
+                <p className="keywords"> <b>Keywords::</b> {item.key_words}</p>
               </div>}
             </button>
           </div>
