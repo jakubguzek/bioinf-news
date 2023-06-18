d9e55979b6537c504fd734bfcb1aeb7f384e7303
jakubguzek <jakub.j.guzek@gmail.com>
2023-06-18 19:27:18 +0200

    add: some functionality from ArticleList to Article.js

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,8 +1,27 @@
 import React from "react"
 
 function Article(props) {
+  const {item, index} = props;
+  
+  const [article, setArticle] = React.useState({});
+  const [visible, setVisible] = React.useState(false);
+
   return (
-    <main>
-    </main>
+      <div>
+        <button className="article-collapsed-button" onClick={console.log("Click")}>
+          <div className="article-collapsed-entry" key={item.doi}>
+            <span className="index">{index + 1}.</span>
+            <span className="title">{item.title}</span>
+            <span className="pub-date">{item.publication_date}</span>
+            <span className="source">{item.source}</span>
+          </div>
+          {visible[index] && <div className="article-entry">
+            <hr/>
+            <p className="authors"><b>Authors:</b> {article[index].authors}</p>
+            <p className="abstract"> <b>Abstract:</b> {article[index].article_abstract}</p>
+            <p className="keywords"> <b>Keywords::</b> {article[index].key_words}</p>
+          </div>}
+        </button>
+      </div>
   )
 }
