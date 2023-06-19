a83baf0acea1b8a4dfce9d836415612b53becf12
jakubguzek <jakub.j.guzek@gmail.com>
2023-06-19 16:21:18 +0200

    change: doi url styling

--- frontend/src/components/ArticleBody.js
+++ frontend/src/components/ArticleBody.js
@@ -2,12 +2,18 @@ import React from "react"
 import AuthorList from "./AuthorList"
 import KeywordList from "./KeywordList"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
+
 export default function ArticleBody({ article }) {
   return (
     <div className="article-details">
       <h3 className="full-title">{article.title}</h3>
-      <p className="doi"><b>DOI: </b> 
-        <a href={`https://${article.doi}`.replace("doi:", "doi.org//")}>{article.doi}</a>
+      <p className="doi"><b>DOI: </b>
+        <a href={`https://${article.doi}`.replace("doi:", "doi.org//")} target="_blank" title="open in new tab" rel="noreferrer">
+          {article.doi}
+        </a>
+        <sup><FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2xs" /></sup>
       </p>
       <p className="authors"><b>Authors: </b><AuthorList authors={article.authors} /></p>
       <p className="abstract"> <b>Abstract:</b> {article.article_abstract}</p>
