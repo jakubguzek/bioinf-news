99dc740856803f116d460ef8aafeddfa0f8dc7ec
jakubguzek <jakub.j.guzek@gmail.com>
2023-06-20 15:44:33 +0200

    add: SearchBar component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Routes, Route, useNavigate} from 'react-router-dom';
 import RandomArticle from "./RandomArticle";
 import KeywordSelect from "./KeywordSelect";
+import SearchBar from "./SearchBar";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -14,6 +15,7 @@ export default function Header() {
     <div className="header">
       <h1> Recent papers in bioinformatics </h1>
       <button onClick={navigateToRandom}>Random article</button>
+      <SearchBar />
       <KeywordSelect />
     </div>
   )
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import AsyncSelect from "react-select/async";
+
+export default function SearchBar() {
+  const [query, setQuery] = React.useState();
+
+  async function loadOptions(value) {
+    const url = (value === null) ?
+      "http://127.0.0.1:8080/articles" :
+      `http://127.0.0.1:8080/articles?query=${value}`
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => data.map(k => ({ value: k.title, label: k.title })));
+  }
+
+  function debugSelect(opt) {
+    setQuery(opt)
+    console.log(setQuery)
+  }
+
+  return (
+    <div className="select-box">
+      <AsyncSelect
+        loadOptions={loadOptions}
+        closeMenuOnSelect={false}
+        onChange={opt => debugSelect(opt)}
+        placeholder="Title..."
+        styles={{
+          placeholder: (baseStyles, state) => ({
+            ...baseStyles,
+            display: 'flex',
+          }),
+        }}
+      />
+    </div>
+  )
+}
