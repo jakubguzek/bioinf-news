61a9b10ef931ff37fb5d512c34c8696921503a9f
jakubguzek <jakub.j.guzek@gmail.com>
2023-06-20 13:39:55 +0200

    add: fetching keywords from database

--- frontend/src/components/KeywordSelect.js
+++ frontend/src/components/KeywordSelect.js
@@ -1,31 +1,35 @@
 import React from "react";
 
-import Select from "react-select";
+import AsyncSelect from "react-select/async";
 
 export default function KeywordSelect() {
-  const availibleKeywords = [
-    { label: "Bioinformatics", value: "Bioinformatics" },
-    { label: "Biotechnology", value: "Biotechnology" },
-    { label: "Mathematics", value: "Mathematics" },
-    { label: "Cell Biology", value: "Cell Biology" }
-  ];
-
   const [chosenKeyowrds, setChosenKeywords] = React.useState([]);
+
+  async function loadOptions() {
+    return fetch("http://127.0.0.1:8080/keywords")
+      .then(response => response.json())
+      .then(data => data.map(k => ({ value: k, label: k })));
+  }
+
+  function debugSelect(opt) {
+    setChosenKeywords(opt)
+    console.log(chosenKeyowrds)
+  }
 
   return (
     <div className="search-bar-box">
-      <Select
-        options = {availibleKeywords}
+      <AsyncSelect
+        loadOptions={loadOptions}
         closeMenuOnSelect={false}
         isMulti
-        onChange={opt => setChosenKeywords(opt)}
+        onChange={opt => debugSelect(opt)}
         placeholder="Keyword"
         styles={{
-            placeholder: (baseStyles, state) => ({
-              ...baseStyles,
-              display: 'flex',
-            }),
-          }}
+          placeholder: (baseStyles, state) => ({
+            ...baseStyles,
+            display: 'flex',
+          }),
+        }}
       />
     </div>
   )
