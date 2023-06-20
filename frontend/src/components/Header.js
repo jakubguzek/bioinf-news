71899c8b27df910f31cc06e532e84a452f38a291
pampuchpampuch <paulinakucharewicz2@gmail.com>
2023-06-20 04:57:34 +0200

    add: ugly random article

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import RandomArticle from "./RandomArticle";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const navigateToRandom = () => {
+    navigate('/random');
+  };
+
   return (
     <div className="header">
-      <h1> Recent papers in bioinformatics </h1>  
+      <h1> Recent papers in bioinformatics </h1>
+      <button onClick={navigateToRandom}>Random article</button>
       <SearchBar />
     </div>
   )
