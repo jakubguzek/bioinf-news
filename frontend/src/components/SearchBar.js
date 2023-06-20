2d2b1e9480aae7f4370c89ff8bb65317f1a7e207
jakubguzek <jakub.j.guzek@gmail.com>
2023-06-20 17:46:03 +0200

    add: foundation for filtering

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -24,6 +24,7 @@ export default function SearchBar() {
       <AsyncSelect
         loadOptions={loadOptions}
         closeMenuOnSelect={false}
+        isClearable
         onChange={opt => debugSelect(opt)}
         placeholder="Title..."
         styles={{
